22d4b2754311ab3a0faab0b833c6bfc0baf66395
Jasper <jasper.bartscher@cloudogu.com>
2022-08-02 16:59:06 +0200

    Implement generic archive-interaction features Add first half of tests.

--- log/archive.go
+++ log/archive.go
@@ -7,41 +7,126 @@ import (
 	"os"
 )
 
-const (
-	supportArchiveFileName = "support-archive.zip"
-)
+type fileCreator interface {
+	create(filename string) (*os.File, error)
+}
+type fileOpener interface {
+	open(filename string) (*os.File, error)
+}
+type defaultFileHandler struct{}
 
-// TODO: remove me; Anleitung fileszippen: https://golang.cafe/blog/golang-zip-file-example.html
-func WriteSupportArchive(logfilePaths []string) error {
-	zippedArchiveFile, err := os.Create(supportArchiveFileName)
+func (d *defaultFileHandler) open(filename string) (*os.File, error) {
+	return os.Open(filename)
+}
+
+func (d *defaultFileHandler) create(filename string) (*os.File, error) {
+	return os.Create(filename)
+}
+
+type ArchiveHandler interface {
+	Open(path string) (io.Writer, error)
+	AppendFileToArchive(path string, writer zip.Writer) error
+	Close() error
+}
+
+// SupportArchiveHandler
+// The normal procedure should look like this.
+// 		1. CreateZipArchiveFile
+// 		2. InitialiseZipWriter
+// 		3. AppendFileToArchive (n times)
+// 		4. Close
+type SupportArchiveHandler struct {
+	writer      *zip.Writer
+	fileCreator fileCreator
+	fileOpener  fileOpener
+}
+
+func New() *SupportArchiveHandler {
+	return &SupportArchiveHandler{
+		fileCreator: &defaultFileHandler{},
+		fileOpener:  &defaultFileHandler{},
+	}
+}
+
+// CreateZipArchiveFile creates the file that will be the zip archive.
+// The zipFilePath expects a complete path with the correct file extension (.zip).
+// If you not intend to create an io.Writer beforehand this method can be the input of InitialiseZipWriter.
+func (ar *SupportArchiveHandler) CreateZipArchiveFile(zipFilePath string) (io.Writer, error) {
+	zippedArchiveFile, err := ar.fileCreator.create(zipFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return zippedArchiveFile, nil
+}
+
+// InitialiseZipWriter takes an existing io.Writer and initializes a zip.Writer based on it.
+func (ar *SupportArchiveHandler) InitialiseZipWriter(zipFile io.Writer) {
+	zipWriter := zip.NewWriter(zipFile)
+	ar.writer = zipWriter
+}
+
+// AppendFileToArchive takes a path to file that is read and appended to an archive.
+// make sure to call the Close method when you're done with appending files to the archive.
+func (ar *SupportArchiveHandler) AppendFileToArchive(fileToZipPath string, filepathInZip string) error {
+	file, err := ar.fileOpener.open(fileToZipPath)
 	if err != nil {
 		return err
 	}
-	zipWriter := zip.NewWriter(zippedArchiveFile)
+	defer file.Close()
 
-	for i, file := range logfilePaths {
-		fmt.Printf("opening file: %v, %s\n", i, file)
-		doguLogFile, err := SelectLogFile(file)
-		if err != nil {
-			panic(err)
-		}
-		defer func() {
-			err = doguLogFile.file.Close()
-		}()
-
-		createdFileInZip, err := zipWriter.Create(file)
-		if err != nil {
-			panic(err)
-		}
-
-		if _, err := io.Copy(createdFileInZip, doguLogFile.file); err != nil {
-			panic(err)
-		}
-
+	fileInZip, err := ar.writer.Create(filepathInZip)
+	if err != nil {
+		return err
 	}
-	defer func() {
-		err = zippedArchiveFile.Close()
-	}()
 
-	return err
+	if _, err := io.Copy(fileInZip, file); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ar *SupportArchiveHandler) Close() error {
+	err := ar.writer.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ar *SupportArchiveHandler) WriteLogFilesIntoArchive(filePaths []string, closeAfterFinish bool) {
+	for _, filePath := range filePaths {
+		ar.WriteLogFileIntoArchive(filePath)
+	}
+	if closeAfterFinish {
+		defer func() {
+			err := ar.Close()
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+}
+
+// WriteLogFileIntoArchive Takes the path to a single logfile and write it to an initialized and created zip-archive.
+// The zipped file's dir structure matches the on the real filesystem.
+func (ar *SupportArchiveHandler) WriteLogFileIntoArchive(filePath string) error {
+
+	fmt.Printf("opening file: %s\n", filePath)
+	doguLogFile, err := SelectLogFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer doguLogFile.file.Close()
+
+	createdFileInZip, err := ar.writer.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := io.Copy(createdFileInZip, doguLogFile.file); err != nil {
+		panic(err)
+	}
+
+	return nil
 }
--- log/archive_test.go
+++ log/archive_test.go
@@ -1,20 +1,70 @@
 package log
 
 import (
+	"fmt"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func teardown() {
-	os.Remove(supportArchiveFileName)
+type mockFileCreator struct{}
+type mockFailedFileCreator struct{}
+
+func (mfc *mockFileCreator) create(filename string) (*os.File, error) {
+	return ioutil.TempFile("", filename)
 }
 
-func TestWriteSupportArchive(t *testing.T) {
-	logfiles := []string{"resources/test/testlogfile.log"}
-	err := WriteSupportArchive(logfiles)
-	if err != nil {
-		assert.NotNil(t, err)
+func (mfc *mockFailedFileCreator) create(filename string) (*os.File, error) {
+	return nil, errors.New("failed to create file")
+}
+
+func TestSupportArchiveHandler_CreateZipArchiveFile(t *testing.T) {
+	//test success
+	handler := SupportArchiveHandler{fileCreator: &mockFileCreator{}}
+	file, err := handler.CreateZipArchiveFile("test.zip")
+	assert.NoError(t, err)
+	assert.NotNil(t, file)
+	// test failure
+	handler = SupportArchiveHandler{fileCreator: &mockFailedFileCreator{}}
+	_, err = handler.CreateZipArchiveFile("test.zip")
+	require.Error(t, err)
+}
+
+func TestSupportArchiveHandler_New(t *testing.T) {
+	supportArchiveHandler := New()
+	assert.NotNil(t, supportArchiveHandler)
+}
+
+func TestSupportArchiveHandler_InitialiseZipWriter(t *testing.T) {
+	handler := SupportArchiveHandler{fileCreator: &mockFileCreator{}}
+	file, _ := handler.CreateZipArchiveFile("test.zip")
+	handler.InitialiseZipWriter(file)
+
+	assert.NotNil(t, handler.writer)
+}
+
+func TestSupportArchiveHandler_AppendFileToArchive(t *testing.T) {
+	handler := SupportArchiveHandler{
+		fileCreator: &mockFileCreator{},
+		fileOpener:  &defaultFileHandler{},
 	}
-	teardown()
+	file, _ := handler.CreateZipArchiveFile("test.zip")
+	handler.InitialiseZipWriter(file)
+
+	tmpFile, err := ioutil.TempFile("", "test.file")
+	if err != nil {
+		fmt.Println("Failed to create temp file: ", tmpFile.Name())
+		t.Fail()
+	}
+	fmt.Println("Created temp file: ", tmpFile.Name())
+	defer tmpFile.Close()
+	if _, err := tmpFile.WriteString("test data"); err != nil {
+		fmt.Print("Unable to write to temporary file")
+		t.Fail()
+	}
+	handler.AppendFileToArchive(tmpFile.Name(), "/test.file")
+	//TODO prüfen ob zip geschrieben wurde
 }
