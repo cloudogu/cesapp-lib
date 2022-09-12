084bf26718d8f0788ef1808a4dc2a7db65b0488a
Simon Klein <simon.klein@cloudogu.com>
2022-09-12 15:27:23 +0200

    #6 Add doc comments

--- registry/Node.go
+++ registry/Node.go
@@ -4,17 +4,24 @@ import (
 	"strings"
 )
 
+// Node Represents the structure (key/value/dir) of the etcd
 type Node struct {
+	// SubNodes contains all direct children of the current node.
 	SubNodes []Node
-	IsDir    bool
-	FullKey  string
-	Value    string
+	// IsDir is true if the current node is a dir, false if the node is a key
+	IsDir bool
+	// FullKey is the full path of a key including the path of all its parents.
+	FullKey string
+	// Value is the value of the node - empty string if the node is a directory
+	Value string
 }
 
+// HasSubNodes returns true if the SubNodes attribute has values, otherwise false
 func (drn Node) HasSubNodes() bool {
 	return len(drn.SubNodes) > 0
 }
 
+// Key Returns the direct key of the node - parent paths are excluded
 func (drn Node) Key() string {
 	if strings.Contains(drn.FullKey, "/") {
 		splitted := strings.Split(drn.FullKey, "/")
@@ -24,6 +31,7 @@ func (drn Node) Key() string {
 	return drn.FullKey
 }
 
+// SubNodeByName Searches in the direct sub nodes for a node with the given key and returns it if found. Returns empty node otherwise.
 func (drn Node) SubNodeByName(key string) Node {
 	for _, subnode := range drn.SubNodes {
 		if subnode.Key() == key {
