87919bb350f870b80b217119a9440798ef6b0842
Philipp Pixel <boris.schaa@cloudogu.com>
2023-03-07 14:58:51 +0100

    #25 Add and improve core.Dogu.Volumes related doc comments

--- core/dogu_v2.go
+++ core/dogu_v2.go
@@ -8,27 +8,97 @@ import (
 )
 
 // VolumeClient adds additional information for clients to create volumes.
+//
+// Example:
+//
+//   {
+//     "Name": "k8s-dogu-operator",
+//     "Params": {
+//       "Type": "configmap",
+//       "Content": {
+//         "Name": "k8s-ces-menu-json"
+//       }
+//     }
+//   }
 type VolumeClient struct {
-	// Name defines the actual client responsible to process this volume definition.
+	// Name identifies the client responsible to process this volume definition. This field is mandatory.
+	//
+	// Examples:
+	//   - cesapp
+	//   - k8s-dogu-operator
 	Name string
-	// Params contains generic data only known by the client.
+	// Params contains generic data only interpretable by the client. This field is mandatory.
 	Params interface{}
 }
 
-// Volume is the volume struct of a dogu and will be used to define docker
-// volumes
+// Volume defines container volumes that are created during the dogu creation or upgrade.
+//
+// Examples:
+//   - { "Name": "data", "Path":"/usr/share/yourtool/data", "Owner":"1000", "Group":"1000", "NeedsBackup": true}
+//   - { "Name": "temp", "Path":"/tmp", "Owner":"1000", "Group":"1000", "NeedsBackup": false}
+//
 type Volume struct {
-	// Name identifies the volume.
+	// Name identifies the volume. This field is mandatory. It must be unique in all volumes of the same dogu.
+	//
+	// The name syntax comply with the file system syntax of the respective host operating system and is encouraged to
+	// consist of:
+	//   - lower case latin characters
+	//   - special characters underscore "_", minus "-"
+	//   - ciphers 0-9
+	//
+	// The name must not be "_private" to avoid conflicts with the dogu's private key.
+	//
+	// Examples:
+	//   - tooldata
+	//   - tool-data-0
+	//
 	Name string
-	// Path to the directory or file where the volume will be mounted inside the dogu.
+	// Path to the directory or file where the volume will be mounted inside the dogu. This field is mandatory.
+	//
+	// Path may consist of several directory levels, delimited by a forward slash "/". Path must comply with the file
+	// system syntax of the container operating system.
+	//
+	// The path must not match `/private` to avoid conflicts with the dogu's private key.
+	//
+	// Examples:
+	//   - /usr/share/yourtool
+	//   - /tmp
+	//   - /usr/share/license.txt
+	//
 	Path string
-	// Owner contains the uid of the user owning this volume.
+	// Owner contains the numeric Unix UID of the user owning this volume. This field is optional.
+	//
+	// For security reasons it is strongly recommended to set the Owner of the volume to an unprivileged user. Please
+	// note that container image must be then built in a way that the container process may own the path either by
+	// user or group ownership.
+	//
+	// The owner syntax must consist of ciphers (0-9) only.
+	//
+	// Examples:
+	//   - "1000" - an unprivileged user
+	//   - "0" - the root user
+	//
 	Owner string
-	// Group contains the gid of the group owning this volume.
+	// Group contains the numeric Unix GID of the group owning this volume. This field is optional.
+	//
+	// For security reasons it is strongly recommended to set the Group of the volume to an unprivileged Group. Please
+	// note that container image must be then built in a way that the container process may own the path either by
+	// user or group ownership.
+	//
+	// The Group syntax must consist of ciphers (0-9) only.
+	//
+	// Examples:
+	//   - "1000" - an unprivileged group
+	//   - "0" - the root group
+	//
 	Group string
-	// NeedsBackup defines if backups need to be created for the volume.
+	// NeedsBackup controls whether the Cloudogu EcoSystem backup facility backs up the whole the volume or not. This
+	// field is optional. If unset a value of `false` will be assumed.
 	NeedsBackup bool
-	// Clients adds client-specific configurations for the volume.
+	// Clients contains a list of client-specific (t. i., the client that interprets the dogu.json) configurations for
+	// the volume. This field is optional.
+	//
+	//
 	Clients []VolumeClient `json:"Clients,omitempty"`
 }
 
@@ -378,7 +448,7 @@ type Dogu struct {
 	//
 	//   [ { "Type": "tcp", "Container": "2222", "Host":"2222" } ]
 	//
-	ExposedPorts         []ExposedPort
+	ExposedPorts []ExposedPort
 	// ExposedCommands defines actions which can be executed in different phases of the dogu lifecycle automatically
 	// triggered by a dogu client like the cesapp or the k8s-dogu-operator or manually from an administrative user.
 	// This field is optional.
@@ -460,7 +530,17 @@ type Dogu struct {
 	//     }
 	//   ],
 	//
-	ExposedCommands      []ExposedCommand
+	ExposedCommands []ExposedCommand
+	// Volumes contains a list of core.Volume which defines [OCI container volumes]. This field is optional.
+	//
+	// Volumes are created during the dogu creation or upgrade. Volumes provide a performance boost compared to
+	// in-container storage.
+	//
+	// Examples:
+	//   [ { "Name": "temp", "Path":"/tmp", "Owner":"1000", "Group":"1000", "NeedsBackup": false} ]
+	//
+	// [OCI container volumes]: https://opencontainers.org/
+	//
 	Volumes              []Volume
 	HealthCheck          HealthCheck // deprecated use HealthChecks
 	HealthChecks         []HealthCheck
