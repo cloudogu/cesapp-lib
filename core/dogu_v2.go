d933112b766912058aee33385303d5cb3881e14c
Robert Auer <robert.auer@cloudogu.com>
2023-03-28 10:48:20 +0200

    Explain advantages of volumes

--- core/dogu_v2.go
+++ core/dogu_v2.go
@@ -819,8 +819,12 @@ type Dogu struct {
 	ExposedCommands []ExposedCommand
 	// Volumes contains a list of [Volume] which defines [OCI container volumes]. This field is optional.
 	//
-	// Volumes are created during the dogu creation or upgrade. Volumes provide a performance boost compared to
-	// in-container storage.
+	// Volumes are created during the dogu creation or upgrade.
+	//
+	// All dynamic data of a Dogu should be stored inside volumes. This holds multiple advantages:
+	//   - Data inside volumes is not lost when the Dogu is upgraded.
+	//   - Data inside volumes can be backed up and restored. See [needsbackup] flag.
+	//   - The access to data stored inside volumes is much faster than to data stored inside the Dogu container.
 	//
 	// Examples:
 	//   [ { "Name": "temp", "Path":"/tmp", "Owner":"1000", "Group":"1000", "NeedsBackup": false} ]
