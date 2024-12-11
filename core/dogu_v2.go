435804c6ca8350e6f784ae31b1753c55625956ec
Philipp Pixel <boris.schaa@cloudogu.com>
2024-12-11 15:14:24 +0100

    #53 Move ValidateSecurity within same file for better readability

--- core/dogu_v2.go
+++ core/dogu_v2.go
@@ -1434,25 +1434,6 @@ func (d *Dogu) CreateV1Copy() DoguV1 {
 	return dogu
 }
 
-// DoguJsonV2FormatProvider provides methods to format Dogu results compatible to v2 API.
-type DoguJsonV2FormatProvider struct{}
-
-// GetVersion returns DoguApiV2 for this implementation.
-func (d *DoguJsonV2FormatProvider) GetVersion() DoguApiVersion {
-	return DoguApiV2
-}
-
-// ReadDoguFromString reads a dogu from a string and returns the API v2 representation.
-func (d *DoguJsonV2FormatProvider) ReadDoguFromString(content string) (*Dogu, error) {
-	var dogu *Dogu
-	err := json.Unmarshal([]byte(content), &dogu)
-	if err != nil {
-		return nil, err
-	}
-
-	return dogu, nil
-}
-
 // ValidateSecurity checks the dogu's Security section for configuration errors.
 func (d *Dogu) ValidateSecurity() error {
 	var errs error
@@ -1476,6 +1457,25 @@ func (d *Dogu) ValidateSecurity() error {
 	}
 
 	return nil
+}
+
+// DoguJsonV2FormatProvider provides methods to format Dogu results compatible to v2 API.
+type DoguJsonV2FormatProvider struct{}
+
+// GetVersion returns DoguApiV2 for this implementation.
+func (d *DoguJsonV2FormatProvider) GetVersion() DoguApiVersion {
+	return DoguApiV2
+}
+
+// ReadDoguFromString reads a dogu from a string and returns the API v2 representation.
+func (d *DoguJsonV2FormatProvider) ReadDoguFromString(content string) (*Dogu, error) {
+	var dogu *Dogu
+	err := json.Unmarshal([]byte(content), &dogu)
+	if err != nil {
+		return nil, err
+	}
+
+	return dogu, nil
 }
 
 // ReadDogusFromString reads multiple dogus from a string and returns the API v2 representation.
